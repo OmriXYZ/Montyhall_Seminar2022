fd6f4b838a5ff4d79fcc3d0ba1a1a2932600e261
Itay Mortov <Itaymortov@gmail.com>
2022-11-07 11:47:59 +0200

    added sounds

--- Seminar/GameModel.py
+++ Seminar/GameModel.py
@@ -1,11 +1,16 @@
 import random
 from tkinter import PhotoImage
+from SoundManager import SoundManager
+import pygame
+from pygame.mixer import Sound
+pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=512)
+pygame.mixer.init()
 
 
 class GameModel:
     def __init__(self, controller):
         self.controller = controller
-
+        self.soundManger = SoundManager()
         self.l1 = ["goat", "car", "goat"]
         random.shuffle(self.l1)
         self.ci = self.l1.index("car")
@@ -25,19 +30,26 @@ class GameModel:
 
         if self.stage == 2:
             self.reset_game()
+            self.soundManger.goat.stop()
+            self.soundManger.win.stop()
+            self.soundManger.lose.stop()
             return
         if self.stage == 1:
             if door_index == self.l2[0]:
                 print("You cant choose goat in number: ", self.l2[0])
                 return
             elif door_index == self.ci:
+                self.soundManger.click.play()
                 self.controller.toplbl_change_lbl("You Win the car, tap any door to play again")
                 self.controller.btn_change_image(self.ci, self.car_photo)
                 self.wins += 1
+                self.soundManger.win.play()
             else:
+                self.soundManger.click.play()
                 self.controller.toplbl_change_lbl("You choose the goat again, you lose, tap any door to play again")
                 self.controller.btn_change_image(door_index, self.goat_photo)
                 self.losses += 1
+                self.soundManger.lose.play()
 
             self.stage += 1
 
@@ -45,15 +57,19 @@ class GameModel:
             # print("ratio: ", self.wins/self.losses)
             return
         if self.stage == 0:
+            self.soundManger.click.play()
             for i in range(len(self.l1)):
                 if i != self.ci and i != door_index:
                     self.l2.append(i)
             if self.l2 == 2:
                 random.shuffle(self.l2)
-            self.controller.toplbl_change_lbl("There is a goat in: {door}\nDo you want to keep your choice or change it?".format(door=self.l2[0]))
+            self.controller.toplbl_change_lbl("There is a goat in: {door}\nDo you want to keep your choice or change "
+                                              "it?".format(door=self.l2[0]))
             self.controller.btn_change_image(self.l2[0], self.goat_photo)
             self.controller.btn_change_lbl(self.l2[0], "goat")
+            self.soundManger.goat.play()
             self.stage += 1
+
 
     def door_selection_simulation(self, door_index):
         if self.stage == 2:
@@ -127,3 +143,5 @@ class GameModel:
             self.door_selection_simulation(choice)
             self.door_selection_simulation(choice)
         self.controller.stats_change_lbl(self.wins, self.losses)
+
+
